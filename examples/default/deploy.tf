9ec4bc929454c5e4de1659edb68705d66b569c83
Mahidhar Reddy <mahidhar@outlook.com>
2021-05-12 15:50:46 -0400

    test: emr deploy

--- examples/default/deploy.tf
+++ examples/default/deploy.tf
@@ -11,17 +11,18 @@ resource "random_string" "this" {
 }
 
 module "vpc" {
-  source               = "terraform-aws-modules/vpc/aws"
-  version              = "2.70.0"
-  name                 = random_string.this.result
-  cidr                 = "10.0.0.0/16"
-  azs                  = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway   = true
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  enable_dhcp_options  = true
+  source                   = "terraform-aws-modules/vpc/aws"
+  version                  = "2.70.0"
+  name                     = random_string.this.result
+  cidr                     = "10.0.0.0/16"
+  azs                      = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
+  private_subnets          = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets           = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway       = true
+  enable_dns_hostnames     = true
+  enable_dns_support       = true
+  enable_dhcp_options      = true
+  dhcp_options_domain_name = "ca-central-1.compute.internal"
 
 }
 
